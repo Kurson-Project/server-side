c75c4d88e4e7611e590042964313ad7e66d1de55
yohanesokta <yohanesoktayt72@gmail.com>
2024-12-31 04:58:04 +0700

    move to mongodb

---
+++ controller/userControler.js
@@ -0,0 +1,5 @@
+export class userControler {
+    home(request,response) {
+        response.json({messgae : "in home"})
+    }
+}
--- routes/user/index.js
+++ routes/user/index.js
@@ -1,8 +1,9 @@
 import express from "express"
+import { userControler } from "../../controller/userControler.js"
+const userControlers = new userControler()
+
 const userRouter = express.Router()
 
-userRouter.get('/',(req,res)=>{
-    res.send("homepage")
-})
+userRouter.get('/',userControlers.home)
 
 export default userRouter
